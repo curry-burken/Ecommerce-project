6f0a41aac7c6f9561cfa51b5bb4ed608567f609a
Pranav <jpranavniran@gmail.com>
2025-04-02 23:52:57 +0530

    navbar created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import { BrowserRouter, Routes, Route }from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import Shop from './Pages/Shop';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar/> 
+        <Routes>
+          <Route path="/" element={<Shop/>}/>
+          <Route path="/mens" element={<Shop category="mens"/>}/>
+          <Route path="/womens" element={<Shop category="womens"/>}/>
+          <Route path="/kids" element={<Shop category="kids"/>}/>
+          <Route path="/product" element={<Product/>}>
+            <Route path=":productId" element={<Product/>}/>
+          </Route>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/login" element={<LoginSignup/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 
 function Navbar() {
+  const[menu,setMenu] = useState("shop");
   return (
     <div className='navbar'>
         <div className='nav-logo'>
@@ -11,14 +13,14 @@ function Navbar() {
             <p>Cloth Shop</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop <hr/></li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:"none"}} to="/">Shop</Link>{menu==="shop"?<hr/>:null}</li>
+            <li onClick={()=>{setMenu("men")}}><Link style={{textDecoration:"none"}} to="/mens">Mens</Link>{menu==="men"?<hr/>:null}</li>
+            <li onClick={()=>{setMenu("women")}}><Link style={{textDecoration:"none"}} to="/womens">Womens</Link>{menu==="women"?<hr/>:null}</li>
+            <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration:"none"}} to="/kids">Kids</Link>{menu==="kids"?<hr/>:null}</li>
         </ul>
         <div className='nav-login-cart'>
-            <button>Login</button>
-            <img src={cart_icon} alt=""/>
+            <Link to="/login"><button>Login</button></Link>
+            <Link to="/cart"><img src={cart_icon} alt=""/></Link>
             <div className='nav-cart-count'>0</div>
 
         </div>
