9a8b49a6f35587e32a1018af7ce2f1536c73367d
Pranav <jpranavniran@gmail.com>
2025-04-04 00:24:24 +0530

    almost done

--- frontend/src/Components/CartItems/CartItems.js
+++ frontend/src/Components/CartItems/CartItems.js
@@ -4,7 +4,7 @@ import { ShopContext } from '../../Context/ShopContext';
 import remove_icon from '../Assets/cart_cross_icon.png';
 
 function CartItems() {
-    const {all_product, cartItems, addToCart, removeFromCart} = useContext(ShopContext);
+    const {getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart} = useContext(ShopContext);
   return (
     <div className='cartitems'>
       <div className='cartitems-format-main'>
@@ -40,7 +40,7 @@ function CartItems() {
             <div>
                 <div className='cartitems-total-item'>
                     <p>Subtotal</p>
-                    <p>${0}</p>
+                    <p>${getTotalCartAmount()}</p>
                 </div>
                 <hr/>
                 <div className='cartitems-total-item'>
@@ -50,10 +50,17 @@ function CartItems() {
                 <hr/>
                 <div className='cartitems-total-item'>
                     <h3>Total</h3>
-                    <h3>${0}</h3>
+                    <h3>${getTotalCartAmount()}</h3>
                 </div>
             </div>
-            <button>PROCEED to CHECKOUT</button>
+            <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className='cartitems-promocode'>
+            <p>If you have a promo code, enter it here.</p>
+            <div className='cartitems-promobox'>
+                <input type="text" placeholder='Promo code'/>
+                <button>Submit</button>
+            </div>
         </div>
       </div>
     </div>
